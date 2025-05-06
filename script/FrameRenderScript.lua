43c5223843b870d1424b012fe9c9acb780847de6
1l0 <rh.plus001@gmail.com>
2025-05-06 19:04:54 +0900

    TODO: reuse prev data

--- script/FrameRenderScript.lua
+++ script/FrameRenderScript.lua
@@ -1,43 +1,43 @@
 local num = CustomTool1.NumberIn1
 if num == 0 then
-    self.StyledText = "-" -- pau
-    setData("prevText", self.StyledText)
+    self.StyledText = "" -- pau
+    self:setData("prevText", self.StyledText)
 elseif num == 1 then
-    self.StyledText = "-" -- N
-    setData("prevText", self.StyledText)
+    self.StyledText = "" -- N
+    self:setData("prevText", self.StyledText)
 elseif num == 2 then
-    self.StyledText = "□" -- a
-    setData("prevText", self.StyledText)
+    self.StyledText = "o" -- a
+    self:setData("prevText", self.StyledText)
 elseif num == 3 then
-    self.StyledText = "-" -- cl
-    setData("prevText", self.StyledText)
+    self.StyledText = "" -- cl
+    self:setData("prevText", self.StyledText)
 elseif num == 4 then
-    self.StyledText = "ㅂ" -- e
-    setData("prevText", self.StyledText)
+    self.StyledText = "=" -- e
+    self:setData("prevText", self.StyledText)
 elseif num == 5 then
-    self.StyledText = "ㅂ" -- i
-    setData("prevText", self.StyledText)
+    self.StyledText = "-" -- i
+    self:setData("prevText", self.StyledText)
 elseif num == 6 then
-    self.StyledText = "ﾛ" -- o
-    setData("prevText", self.StyledText)
+    self.StyledText = "O" -- o
+    self:setData("prevText", self.StyledText)
 elseif num == 7 then
-    self.StyledText = "ε" -- u
-    setData("prevText", self.StyledText)
+    self.StyledText = "•" -- u
+    self:setData("prevText", self.StyledText)
 elseif num == 8 then
-    self.StyledText = "□" -- A
-    setData("prevText", self.StyledText)
+    self.StyledText = "o" -- A
+    self:setData("prevText", self.StyledText)
 elseif num == 9 then
-    self.StyledText = "ㅂ" -- E
-    setData("prevText", self.StyledText)
+    self.StyledText = "=" -- E
+    self:setData("prevText", self.StyledText)
 elseif num == 10 then
-    self.StyledText = "ㅂ" -- I
-    setData("prevText", self.StyledText)
+    self.StyledText = "-" -- I
+    self:setData("prevText", self.StyledText)
 elseif num == 11 then
-    self.StyledText = "ﾛ" -- O
-    setData("prevText", self.StyledText)
+    self.StyledText = "O" -- O
+    self:setData("prevText", self.StyledText)
 elseif num == 12 then
-    self.StyledText = "ε" -- U
-    setData("prevText", self.StyledText)
+    self.StyledText = "•" -- U
+    self:setData("prevText", self.StyledText)
 else
-    self.StyledText = getData("prevText")
+    self.StyledText = self:getData("prevText")
 end
